df7c25758cd852fa0de46e3cf5c89b8375e64042
nastyahh <nasstyahus@gmail.com>
2023-12-15 14:07:21 +0300

    ten

--- src/10-opp-tasks.js
+++ src/10-opp-tasks.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /**
  * You will complete the PaginationHelper class,
  * which is a utility class helpful for querying paging information related to an array.
@@ -24,34 +25,43 @@
 class PaginationHelper {
   // The constructor takes in an array of items and a integer indicating how many
   // items fit within a single page
-  constructor(/* collection, itemsPerPage */) {
-    throw new Error('Not implemented');
+  constructor(collection, itemsPerPage) {
+    this.collection = collection;
+    this.itemsPerPage = itemsPerPage;
   }
 
   // returns the number of items within the entire collection
   itemCount() {
-    console.log(this);
-    throw new Error('Not implemented');
+    return this.collection.length;
   }
 
   // returns the number of pages
   pageCount() {
-    console.log(this);
-    throw new Error('Not implemented');
+    return Math.ceil(this.itemCount() / this.itemsPerPage);
   }
 
   // returns the number of items on the current page. page_index is zero based.
   // this method should return -1 for pageIndex values that are out of range
-  pageItemCount() {
-    console.log(this);
-    throw new Error('Not implemented');
+  pageItemCount(pageIndex) {
+    const totalPages = this.pageCount();
+    if (pageIndex < 0 || pageIndex >= totalPages) {
+      return -1;
+    }
+    if (pageIndex === totalPages - 1) {
+      return this.itemCount() % this.itemsPerPage || this.itemsPerPage;
+    }
+    return this.itemsPerPage;
   }
 
   // determines what page an item is on. Zero based indexes
   // this method should return -1 for itemIndex values that are out of range
-  pageIndex() {
-    console.log(this);
-    throw new Error('Not implemented');
+  pageIndex(itemIndex) {
+    if (
+      itemIndex < 0 || itemIndex >= this.itemCount() || this.itemCount() === 0
+    ) {
+      return -1; // Item index out of range or empty collection
+    }
+    return Math.floor(itemIndex / this.itemsPerPage);
   }
 }
 
